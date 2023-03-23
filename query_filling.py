6369c4e92e23b9b604c3679c988e3aeb03bc1557
xdqiang <1519445973@qq.com>
2023-03-23 22:09:52 +0800

    设置选择题选项概率

--- query_filling.py
+++ query_filling.py
@@ -1,9 +1,47 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time,json,random
+from selenium.webdriver import ActionChains
+
+def get_track(distance):      # distance为传入的总距离
+    # 移动轨迹
+    track = []
+    # 当前位移
+    current = 0
+    # 减速阈值
+    mid = distance*4/5
+    # 计算间隔
+    t = 0.2
+    # 初速度
+    v = 1
+
+    while current < distance:
+        if current < mid:
+            # 加速度为2
+            a = random.uniform(0, 2)
+        else:
+            # 加速度为-2
+            a = random.uniform(-1, -2)
+        v0 = v
+        # 当前速度
+        v = v0 + a*t
+        # 移动距离
+        move = v0*t+1/2*a*t*t
+        # 当前位移
+        current += move
+        # 加入轨迹
+        track.append(round(move))
+    return track
+
+def move_to_gap(driver, slider, tracks):     # slider是要移动的滑块, tracks是要传入的移动轨迹
+    ActionChains(driver).click_and_hold(slider).perform()
+    for x in tracks:
+        ActionChains(driver).move_by_offset(xoffset=x, yoffset=0).perform()
+    time.sleep(0.5)
+    ActionChains(driver).release().perform()
 
 #we need a special finction to help circulate here
-def opeDiv(i,driver,obj):
+def opeDiv(i,driver,obj,selNum=None,weights=None):
     lst=list(obj.keys())#获取可以填写的项目
     #step1
     div=driver.find_element(By.ID,"div"+str(i))
@@ -11,6 +49,7 @@ def opeDiv(i,driver,obj):
     typeDiv=div.get_attribute("type")
     #step3
     if typeDiv=="1":
+        # 文本输入框
         #operate as the text input-send_keys(content)
         node=driver.find_element(By.XPATH,'//input[@id="q%s"]'%str(i))
         for i in lst:
@@ -21,8 +60,11 @@ def opeDiv(i,driver,obj):
                 continue
         return
     elif typeDiv=="3":
-        #operate as the radio input-click()
-        node=driver.find_element(By.XPATH,'//*[@id="div%s"]/div[2]/div[1]/span/a'%str(i))
+        # 单选题
+        choice = [1, 2, 3, 4, 5]
+        weight= weights[str(i)]
+        oneC=random.choices(choice, weights=weight)[0]
+        node=driver.find_element(By.XPATH,'//*[@id="div%s"]/div[2]/div[%s]/span/a'%(str(i),str(oneC)))
         node.click()
     elif typeDiv=="4":
         #operate as the checkbox input-click()
@@ -31,36 +73,68 @@ def opeDiv(i,driver,obj):
         #上面两个节点是以四个多选项为例进行的，并且选择的选项是随机的
         node1.click()
         node2.click()
+    elif typeDiv=="6":
+        # 选项
+        choice = [1, 2, 3, 4, 5]
+        # 权重
+        weight= weights[str(i)]
+        num=selNum*2+1
+        weight_id=1
+        for it in range(3,num+1,2):
+            oneC=random.choices(choice, weights=weight[str(weight_id)])[0]
+            node=driver.find_element(By.XPATH,'//*[@id="div%s"]/div[2]/table/tbody/tr[%d]/td[2]/ul/li[%d]/a'%(str(i),it,oneC))
+            node.click()
+            weight_id+=1
+
     else:
         return
 
-def write(link,driver,obj):
+def write(link,driver,obj,weights):
     #通过问卷url获取并打开
     driver.get(link)
     #try filling the query, if not accessible, fill in by hand
-    for i in range(1,13):   #这里实际上可以通过爬虫确定问题数目减少循环次数
+    page1=range(1,7)
+    selNum1=0
+    page2=range(7,8)
+    selNum2=8
+    page3=range(8,9)
+    selNum3=13
+    page4=range(9,10)
+    selNum4=10    
+    pages=[page1, page2, page3, page4]
+    selNums=[selNum1,selNum2,selNum3,selNum4]
+
+    p=0
+    for page in pages:
+        for i in page:   
+            opeDiv(i,driver,obj,selNums[p],weights)
         try:
-            opeDiv(i,driver,obj)
+            # 存在下一页，获取下一页按钮
+            driver.find_element(By.XPATH,'//*[@id="divNext"]/a').click()
+            p+=1
         except:
-            continue
-    nodeOn=driver.find_element(By.XPATH,'//*[@id="ctlNext"]')
-    nodeOn.click()#提交，这一条一般是不变的
+            # 不存在下一页
+            # 用于用甄别有效性
+            driver.find_element(By.XPATH,"//*[@id='div10']/div[2]/div[1]/span/a").click()
+            driver.find_element(By.XPATH,"//*[@id='ctlNext']").click()
+
+            
     try:#尝试点击验证，这个不耽误时间，停留0.1秒就够了
-        time.sleep(0.5)#必要的间歇时间，没有的话可能找不到节点
+        time.sleep(5)#必要的间歇时间，没有的话可能找不到节点
         element = driver.find_element(By.XPATH,'//*[@id="layui-layer1"]/div[3]/a[1]')
         element.click()#点击确认验证
         yanz = driver.find_element(By.XPATH,'//*[@id="SM_BTN_1"]/div[1]/div[4]')
         yanz.click()#点击验证按钮
         print("end time:%d"%time.localtime(time.time())[5])
         print("已点击验证按钮!")#time.localtime(time.time())[5]#到这一步只需要2秒钟
-        time.sleep(5)
     except:
-        time.sleep(60)#为了防止出现任何可能的bug，人工盯防，一旦出现停止程序手动  填写  或  验证
         pass
+    
+    time.sleep(6)
     #下面需要一个判断是否填写成功的方法，当然前台运行浏览器时可以直观看到
     feedback=driver.find_element(By.XPATH,'//*[@id="divdsc"]')
     print(feedback.text)
-    time.sleep(5)
+
 
 def onTime():#下面设置一个定时填写的函数
     #现在需要设置问卷填写的时间，到时间以后再获取链接进行填写即跳出循环
@@ -97,10 +171,15 @@ def openChrome():
 #function main()
 if __name__=="__main__":
     obj=json.load(open("questions.json","r",encoding="utf-8"))#读取json文件获取要填写的信息
-    driver=openChrome()#预加载浏览器
-    link=input("THE LINK OF THE QUERY:\n")#嵌入链接
-    if onTime():
-        print("start time:%d"%time.localtime(time.time())[5])
-        time.sleep(0.01)#防止系统原因出现问卷没有及时开放
-        write(link,driver,obj)
+    weights=json.load(open("weights.json","r",encoding="utf-8"))
+    times=10
+    for each in range(times):
+        #防止系统原因出现问卷没有及时开放
+        driver=openChrome()#预加载浏览器
+        # link=input("THE LINK OF THE QUERY:\n")#嵌入链接
+        link="https://www.wjx.cn/vm/eOMHAci.aspx"
+        # if onTime():
+        # print("start time:%d"%time.localtime(time.time())[5])
+        time.sleep(0.01)
+        write(link,driver,obj,weights)
 
